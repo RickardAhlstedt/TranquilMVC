99e5be89d14cee825bb6ff249502a11f31153e68
Rickard Ahlstedt <rickard.re.ahlstedt@gmail.com>
2018-03-05 21:41:24 +0100

    Updated DB
    Forgot to push the sql.

--- tranquil_db.sql
+++ tranquil_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 03, 2018 at 01:36 PM
+-- Generation Time: Mar 05, 2018 at 09:40 PM
 -- Server version: 10.1.10-MariaDB
 -- PHP Version: 7.0.4
 
@@ -62,7 +62,8 @@ CREATE TABLE `entinfocontent` (
 --
 
 INSERT INTO `entinfocontent` (`contentId`, `contentTitle`, `contentText`, `contentMetaKeywords`, `contentMetaDescription`, `contentMetaCanonicalUrl`, `contentStatus`, `contentCreated`, `contentUpdated`) VALUES
-(1, 'Tranquil', '<p>Basic MVC and CMS</p>\r\n\r\n<div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px">test</div>\r\n\r\n<p>tesatstaws</p>\r\n', 'meta, key, words', 'metadescription', '', 'active', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+(1, 'Tranquil', '<p>Basic MVC and CMS</p>\r\n\r\n<div style="background:#eeeeee; border:1px solid #cccccc; padding:5px 10px">test</div>\r\n\r\n<p>tesatstaws</p>\r\n', 'meta, key, words', 'metadescription', '', 'active', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, 'test-page', '<p>You can include other views by doing:</p>\r\n\r\n<p>{view:infoContent/404}</p>\r\n', '', '', '', 'active', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -76,16 +77,19 @@ CREATE TABLE `entnavigation` (
   `navigationParentId` int(11) NOT NULL,
   `navigationName` varchar(255) NOT NULL,
   `navigationHref` varchar(255) NOT NULL,
-  `navigationBehavior` enum('_self','_blank','_parent','_top') NOT NULL
+  `navigationBehavior` enum('_self','_blank','_parent','_top') NOT NULL,
+  `navigationPrefixContent` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `entnavigation`
 --
 
-INSERT INTO `entnavigation` (`navigationId`, `navigationGroup`, `navigationParentId`, `navigationName`, `navigationHref`, `navigationBehavior`) VALUES
-(1, 'guest', 0, 'Home', '/', '_self'),
-(2, 'guest', 1, 'Sub', '/', '_self');
+INSERT INTO `entnavigation` (`navigationId`, `navigationGroup`, `navigationParentId`, `navigationName`, `navigationHref`, `navigationBehavior`, `navigationPrefixContent`) VALUES
+(1, 'guest', 0, 'Home', '/', '_self', ''),
+(2, 'guest', 1, 'Sub', '/', '_self', ''),
+(3, 'admin', 0, 'Dashboard', '/admin', '_self', '<i class="fas fa-tachometer-alt"></i>'),
+(4, 'admin', 0, 'Pages', '/admin/infoContent', '_self', '<i class="far fa-file-alt"></i>');
 
 -- --------------------------------------------------------
 
@@ -114,7 +118,8 @@ INSERT INTO `entroutes` (`routeId`, `routePath`, `routeTemplate`, `routeModel`, 
 (3, '/admin/login', 'adminLogin', 'admin', 'login', 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
 (4, '/admin/logout', 'admin', 'admin', 'logout', 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
 (5, '/admin/infoContent', 'admin', 'infoContent', 'list', 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(6, '/admin/infoContent/add', 'admin', 'infoContent', 'formAdd', 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+(6, '/admin/infoContent/add', 'admin', 'infoContent', 'formAdd', 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(7, '/test-page', 'default', 'infoContent', 'show', 2, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -138,7 +143,7 @@ CREATE TABLE `entusers` (
 --
 
 INSERT INTO `entusers` (`userId`, `userName`, `userPass`, `userStatus`, `userEmail`, `userLastLogin`, `userCreated`, `userUpdated`) VALUES
-(1, 'developer', '7e7aad44abe3c029bd3a7543520605e1', 'admin', '', '2018-03-02 14:16:39', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+(1, 'developer', '7e7aad44abe3c029bd3a7543520605e1', 'admin', '', '2018-03-05 21:35:25', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 --
 -- Indexes for dumped tables
@@ -194,17 +199,17 @@ ALTER TABLE `entconfig`
 -- AUTO_INCREMENT for table `entinfocontent`
 --
 ALTER TABLE `entinfocontent`
-  MODIFY `contentId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `contentId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `entnavigation`
 --
 ALTER TABLE `entnavigation`
-  MODIFY `navigationId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `navigationId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `entroutes`
 --
 ALTER TABLE `entroutes`
-  MODIFY `routeId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `routeId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `entusers`
 --
